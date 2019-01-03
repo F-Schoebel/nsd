583a4f9b3db316b07fa084bd796f29eec47805be
Florian <xibalba9494@web.de>
2019-01-03 16:50:40 +0100

    Datenbank soweit fertiggestellt / fehlen noch Kontent und Funktionen

--- DevVm.sql
+++ DevVm.sql
@@ -1,17 +1,160 @@
-drop Table SYSTEM.KUNDE;
-drop Table SYSTEM.ENTWICKLER;
-drop Table SYSTEM.SOFTWAREARTIKEL;
-drop Table SYSTEM.WARENKORBEINTRAG;
-drop Type STANDORT_TYP force;
-drop Type STANDORT_LISTE_TYP force;
-drop Type ENTWICKLER_TYP force;
-drop Type KUNDE_TYP force;
-drop Type SOFTWAREARTIKEL_TYP force;
-drop Type Computerspiel_typ force;
-drop Type Anwendungssoftware_typ force;
-drop Type PERSON_TYP force;
-drop Type WARENKORBEINTRAG_TYP force;
+BEGIN
+    EXECUTE IMMEDIATE 'drop Table SYSTEM.KUNDE';
+EXCEPTION
+    WHEN OTHERS THEN NULL;
+END;
+/
 
+BEGIN
+    EXECUTE IMMEDIATE 'drop Table SYSTEM.ENTWICKLER';
+EXCEPTION
+    WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Table SYSTEM.SOFTWAREARTIKEL';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Table SYSTEM.WARENKORBEINTRAG';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Table SYSTEM.PUBLISHER';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Table SYSTEM.BESTELLUNG';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Type BESTELLUNGSEINTRAEGE_NT_TYP force';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Type STANDORT_TYP force';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Type STANDORT_LISTE_TYP force';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Type ENTWICKLER_TYP force';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Type KUNDE_TYP force';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Type SOFTWAREARTIKEL_TYP force';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Type Computerspiel_typ force';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Type Anwendungssoftware_typ force';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Type BESTELLEINTRAEGE_NT_TYP force';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Type BESTELLEINTRAG_TYP force';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Type PERSON_TYP force';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Type WARENKORBEINTRAG_TYP force';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Type PUBLISHER_TYP force';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Type BESTELLUNG_TYP force';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'drop Type BESTELLEINTRAG_TYP force';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+Create Type Person_typ as Object (
+    VORNAME varchar(30),
+    NAME varchar(30),
+    GEBURTSDATUM date,
+    EMAIL varchar(30),
+    TELEFONNUMMER varchar(30),
+    IBAN varchar(20)
+);
+
+/
 
 Create Type Standort_typ as Object (
     LAND varchar(50),
@@ -24,17 +167,6 @@ Create Type Standort_typ as Object (
 /
 
 Create Type Standort_liste_typ as VARRAY(5)of Standort_typ; 
-
-/
-
-Create Type Person_typ as Object (
-    VORNAME varchar(30),
-    NAME varchar(30),
-    GEBURTSDATUM date,
-    EMAIL varchar(30),
-    TELEFONNUMMER varchar(30),
-    IBAN varchar(20)
-);
 
 /
 
@@ -65,16 +197,47 @@ Create Type Softwareartikel_typ as Object (
 
 /
 
+Create Type Publisher_typ as Object (
+    PublisherID int,
+    Firmenname varchar(30),
+    Standorte Standort_liste_typ
+);
+
+/
+
 Create Type Computerspiel_typ under Softwareartikel_typ (
     Altersbegrenzung integer,
     Genre varchar(30),
-    SequelID integer    
+    SequelID integer,
+    Publisher REF Publisher_typ
 );
 
 /
 
 Create Type Anwendungssoftware_typ under Softwareartikel_typ (
     Lizenz varchar(30)    
+);
+
+/
+
+Create Type Bestelleintrag_typ as Object (
+    Artikel REF Softwareartikel_typ,
+    Anzahl int,
+    Einzelpreis double precision
+);
+
+/
+
+CREATE TYPE BESTELLUNGSEINTRAEGE_NT_TYP AS TABLE OF Bestelleintrag_typ;
+
+/
+
+Create Type Bestellung_typ as Object (
+    BestellID int,
+    Kunde REF Kunde_typ,
+    Bestelldatum Date,
+    Status varchar(30),
+    Bestellungen BESTELLUNGSEINTRAEGE_NT_TYP
 );
 
 /
@@ -100,6 +263,12 @@ Create Table SYSTEM.ENTWICKLER OF Entwickler_typ (
 
 /
 
+Create Table SYSTEM.PUBLISHER OF Publisher_typ (
+    PublisherID PRIMARY KEY
+); 
+
+/
+
 Create Table SYSTEM.SOFTWAREARTIKEL (
     ArtikelID Integer PRIMARY KEY,
     Softwareartikel Softwareartikel_typ
@@ -110,6 +279,13 @@ Create Table SYSTEM.SOFTWAREARTIKEL (
 Create Table SYSTEM.WARENKORBEINTRAG OF Warenkorbeintrag_typ (
     EintragID PRIMARY KEY 
 ); 
+
+/
+
+Create Table SYSTEM.BESTELLUNG OF Bestellung_typ (
+    BestellID PRIMARY KEY
+) NESTED TABLE
+    Bestellungen STORE AS BESTELLEINTRAEGE_TAB; 
 
 /
 
