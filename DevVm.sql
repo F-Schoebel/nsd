00813eadfa2bb9c0967d64feb4b868b8e07718a3
Florian <xibalba9494@web.de>
2019-01-17 12:42:29 +0100

    Datenbank fertiggestellt, auch mit Inhalt gefüllt

--- DevVm.sql
+++ DevVm.sql
@@ -13,7 +13,14 @@ end;
 /
 
 begin
-  execute immediate 'drop table system.softwareartikel';
+  execute immediate 'drop table system.anwendungssoftware';
+exception
+  when others then null;
+end;
+/
+
+begin
+  execute immediate 'drop table system.computerspiel';
 exception
   when others then null;
 end;
@@ -138,13 +145,6 @@ exception
 end;
 /
 
-begin
-  execute immediate 'drop type bestelleintrag_typ force';
-exception
-  when others then null;
-end;
-/
-
 create type person_typ as object (
     vorname varchar(30),
     name varchar(30),
@@ -171,7 +171,7 @@ create type standort_liste_typ as varray(5)of standort_typ;
 /
 
 create type kunde_typ as object (
-    kundennr int,
+    kundenid int,
     person person_typ,
     standorte standort_liste_typ
 );
@@ -188,7 +188,7 @@ create type entwickler_typ as object (
 /
 
 create type softwareartikel_typ as object (
-    artikelid integer,
+    artikelid int,
     entwickler ref entwickler_typ,
     softwarename varchar(50),
     einzelpreis double precision,
@@ -208,7 +208,7 @@ create type publisher_typ as object (
 create type computerspiel_typ under softwareartikel_typ (
     altersbegrenzung integer,
     genre varchar(30),
-    sequelid integer,
+    prequelid integer,
     publisher ref publisher_typ
 );
 
@@ -228,7 +228,7 @@ create type bestelleintrag_typ as object (
 
 /
 
-create type bestellungseintraege_nt_typ as table of bestelleintrag_typ;
+create type bestelleintraege_nt_typ as table of bestelleintrag_typ;
 
 /
 
@@ -237,13 +237,13 @@ create type bestellung_typ as object (
     kunde ref kunde_typ,
     bestelldatum date,
     status varchar(30),
-    bestellungen bestellungseintraege_nt_typ
+    bestellungen bestelleintraege_nt_typ
 );
 
 /
 
 create type warenkorbeintrag_typ as object (
-    eintragid int,
+    warenkorbeintragid int,
     anzahl int,
     artikel ref softwareartikel_typ,
     kunde ref kunde_typ
@@ -252,7 +252,7 @@ create type warenkorbeintrag_typ as object (
 /
 
 create table system.kunde of kunde_typ (
-    kundennr primary key
+    kundenid primary key
 );
 
 /
@@ -269,15 +269,20 @@ create table system.publisher of publisher_typ (
 
 /
 
-create table system.softwareartikel (
-    artikelid integer primary key,
-    softwareartikel softwareartikel_typ
+create table system.computerspiel of computerspiel_typ (
+    artikelid primary key
+);
+
+/
+
+create table system.anwendungssoftware of anwendungssoftware_typ (
+    artikelid primary key
 );
 
 /
 
 create table system.warenkorbeintrag of warenkorbeintrag_typ (
-    eintragid primary key
+    warenkorbeintragid primary key
 );
 
 /
@@ -288,6 +293,261 @@ create table system.bestellung of bestellung_typ (
     bestellungen store as bestelleintraege_tab;
 
 /
+
+--------------------------------------------------------------------------------------------------------------------------------
+--Inhalt / Kontent
+--------------------------------------------------------------------------------------------------------------------------------
+
+insert into system.kunde values
+(       
+    kunde_typ
+    ( 
+        1,
+        person_typ
+        (
+            'Leo', 
+            'Tobisch', 
+            to_date('10.01.1993', 'dd.mm.yyyy'),    
+            'leo@w-hs.de', 
+            '0905 666666', 
+            '123456'
+        ), 
+        standort_liste_typ
+        (
+            standort_typ
+            (
+                'Deutschland',
+                'Gelsenkirchen',
+                '12345',
+                'supernstrasse',
+                '434'
+            )
+        )
+    )
+);
+
+/
+
+insert into system.publisher values
+(       
+    publisher_typ
+    ( 
+        1,
+        'Bubisoft',
+        standort_liste_typ
+        (
+            standort_typ
+            (
+                'Deutschland',
+                'Berlin',
+                '75648',
+                'Bubisoftstraße',
+                '21'
+            ),
+            standort_typ
+            (
+                'Belgien',
+                'Brüssel',
+                '92046',
+                'Gropplerstraße',
+                '87'
+            )
+        )    
+    )
+);
+
+/
+
+insert into system.entwickler values
+(       
+    entwickler_typ
+    ( 
+        1,
+        'Super-inc',
+        person_typ
+        (
+            'Flo', 
+            'Smith', 
+            to_date('22.02.1993', 'dd.mm.yyyy'),    
+            'flo@w-hs.de', 
+            '0905 666666', 
+            '123456'
+        ), 
+        standort_liste_typ
+        (
+            standort_typ
+            (
+                'Deutschland',
+                'Gelsenkirchen',
+                '12345',
+                'superstrasse',
+                '431'
+            )
+        )
+    )
+);
+
+/
+
+insert into system.computerspiel values
+(       
+    computerspiel_typ
+    ( 
+        1,
+        (
+            select
+                ref(p) 
+            from
+                system.entwickler p
+            where
+                p.entwicklerid = 1
+        ),
+        'Red Dead Leo',
+        59.99,
+        to_date('10.02.2018','DD.MM.YYYY'),
+        18,
+        'Shooter',
+        null,
+        (
+            select 
+                ref(p)
+            from
+                system.publisher p
+            where
+                p.publisherid = 1
+        )
+    )
+);
+
+/
+
+insert into system.computerspiel values
+(       
+    computerspiel_typ
+    ( 
+        2,
+        (
+            select
+                ref(p) 
+            from
+                system.entwickler p
+            where
+                p.entwicklerid = 1
+        ),
+        'Red Dead Leo 2',
+        59.99,
+        to_date('05.01.2019','DD.MM.YYYY'),
+        18,
+        'Shooter',
+        1,
+        (
+            select 
+                ref(p)
+            from
+                system.publisher p
+            where
+                p.publisherid = 1
+        )
+    )
+);
+
+/
+
+insert into system.warenkorbeintrag values
+(       
+    warenkorbeintrag_typ
+    ( 
+        1,
+        2,
+        (
+            select
+                ref(p) 
+            from
+                system.computerspiel p
+            where
+                p.artikelid = 1
+        ),
+        (
+            select 
+                ref(p)
+            from
+                system.kunde p
+            where
+                p.kundenid = 1
+        )
+    )
+);
+
+/
+
+insert into system.bestellung values
+(       
+    bestellung_typ
+    ( 
+        1,
+        (
+            select
+                ref(p) 
+            from
+                system.kunde p
+            where
+                p.kundenid = 1
+        ),
+        to_date('16.01.2019', 'DD.MM.YYYY'),
+        'erhalten',
+        bestelleintraege_nt_typ
+        (
+            bestelleintrag_typ
+            (
+                (
+                    select
+                        ref(p)
+                    from
+                        system.computerspiel p
+                    where
+                        p.artikelid = 1
+                ),
+                1,
+                59.99
+            ),
+            bestelleintrag_typ
+            (
+                (
+                    select
+                        ref(p)
+                    from
+                        system.computerspiel p
+                    where
+                        p.artikelid = 2
+                ),
+                1,
+                59.50
+            )            
+        )
+    )
+);
+
+/
+
+insert into system.anwendungssoftware values
+(       
+    anwendungssoftware_typ
+    ( 
+        3,
+        (
+            select
+                ref(p) 
+            from
+                system.entwickler p
+            where
+                p.entwicklerid = 1
+        ),
+        'Cheattool xy',
+        10.00,
+        to_date('12.03.2018','DD.MM.YYYY'),
+        'unbegrenzt'
+    )
+);
 
 
 --select treat(person as person_typ).name from kunde
